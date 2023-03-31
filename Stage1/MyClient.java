abf23add933e61d63d7aab4617303823f304eb4a
Adstar123 <adstar3108@gmail.com>
2023-03-31 16:46:38 +1100

    Update MyClient.java

--- Stage1/MyClient.java
+++ Stage1/MyClient.java
@@ -3,7 +3,24 @@ import java.net.*;
 import java.nio.charset.StandardCharsets;
 
 public class MyClient {
-public static void main(String[] args) {
+
+public static String AUTH = "AUTH " + System.getProperty("Adam");
+
+public MyClient() {
+}
+
+public String readMsg(byte[] b, BufferedInputStream bin) {
+    try {
+      bin.read(b);
+      String str = new String(b, StandardCharsets.UTF_8);
+      return str;
+    } catch (Exception e) {
+      System.out.println(e);
+    }
+    return "error";
+  }
+
+	public static void main(String[] args) {
 try {
 	Socket s=new Socket("localhost",50000);
 	DataInputStream dis = new DataInputStream(s.getInputStream());
@@ -11,46 +28,42 @@ try {
     BufferedInputStream bis = new BufferedInputStream(dis);
     BufferedOutputStream bos = new BufferedOutputStream(dos);
 		
-	
+	MyClient myClient = new MyClient();
+
 	bos.write(("HELO\n").getBytes());
 	bos.flush();
 	
 	
-	String str=(String)dis.readLine();
-	System.out.println(str);
+	String serverResponse = myClient.readMsg(new byte[2], bis);
 	
-	bos.write(("AUTH Adam\n").getBytes());
-	bos.flush();
+	bos.write(AUTH.getBytes());
+    bos.flush();
 	
+	serverResponse = myClient.readMsg(new byte[2], bis);
 	
-	str=dis.readLine();
-	System.out.println(str);
 	bos.write(("REDY\n").getBytes());
 	bos.flush();
 	
 
-	str=dis.readLine();
-	System.out.println(str);
+	serverResponse = myClient.readMsg(new byte[64], bis);
 	
-	String[] job = str.split(" ", 10);
+	String[] job = serverResponse.split(" ", 10);
 	bos.write(("GETS All\n").getBytes());
 	bos.flush();
 	
-
-	str = dis.readLine();
-	System.out.println(str);
+	serverResponse = myClient.readMsg(new byte[32], bis);
 	
-	int nRecs  = Integer.parseInt(String.valueOf(str.charAt(5)));
+	int nRecs  = Integer.parseInt(String.valueOf(serverResponse.charAt(5)));
 	System.out.println(nRecs);
 	
 	bos.write(("OK\n").getBytes());
 	bos.flush();
+
+    String str = new String();
 	
 	int j = 0;
 	String[] large = new String[10];
 	for(int i = 0; i < nRecs; i++){
-		str = dis.readLine();
-		System.out.println(str);
 	
 	String[] arr = str.split(" ", 9);
 	int coreNum = Integer.parseInt(String.valueOf(arr[4]));
